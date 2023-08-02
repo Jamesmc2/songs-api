59caaa6fb54c30fe003deeb1a5dc2408297c35dd
jimmy <jamesmc21998@gmail.com>
2023-08-02 14:18:04 -0500

    add route for creating a new song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,4 +8,15 @@ class SongsController < ApplicationController
     @song = Song.find_by(id: params[:id])
     render :show
   end
+
+  def create
+    @song = Song.new(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year],
+    )
+    @song.save
+    render :show
+  end
 end
